19039bf33bd928146fe3e876926c21a74bbb1511
yamawade <mariamawade69@gmail.com>
2025-08-02 01:22:15 +0000

    first commit

---
+++ DevoirSoap/src/main/java/com/groupeisi/DevoirSoap/DevoirSoapApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.DevoirSoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoirSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoirSoapApplication.class, args);
+	}
+
+}
